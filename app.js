51069902da2a6f20bbc4f66aedd1d1d26ebfe469
Rajdeep Malakar <Rajdeep@tgeeks.cf>
2021-09-09 18:56:43 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const request = require('request');
+const shareimage = require('shareimage');
+
+const app = express();
+
+app.listen(process.env.PORT || 5800, () => {
+    console.log("Success")
+})
+
+app.get('/v1/image', (req, res) => {
+    const image = shareimage.get(req.query)
+    request({
+            url: image,
+            encoding: null
+        },
+        (err, resp, buffer) => {
+            if (!err && resp.statusCode === 200) {
+                res.set("Content-Type", resp.headers['content-type']);
+                res.header({
+                    'X-Powered-By': "ShareImage",
+                    "X-Cloud-Name": req.query.cloudName,
+                    'X-Image-Id': req.query.imagePublicID,
+                })
+                res.send(resp.body);
+            }
+        });
+})
